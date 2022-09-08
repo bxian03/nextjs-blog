f3358dfee6b171615818615f233b17ece369254a
Brian Xian <brianxian2607@gmail.com>
2022-09-07 20:14:46 -0700

    editted the formatting

--- pages/index.js
+++ pages/index.js
@@ -22,12 +22,9 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>
-          I'm Brian Xian and I am a student at the British Columbia Institute of
-          Technology in the Computing Information Technology program.
-        </p>
-        <p>
-          (This is a sample website - you'll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          My name is Brian Xian and I am a student at the British Columbia
+          Institute of Technology in the Computing Information Technology
+          program.
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
